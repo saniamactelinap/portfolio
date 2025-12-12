cbee31d24b4f8a6fb13809ce7887bb41a4a45f40
saniamactelinap <saniamactelin.a.p@gmail.com>
2025-12-12 12:11:09 +0530

    Update vite.config.ts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio/", // <-- add this line for GitHub Pages
+  base: mode === "production" ? "/portfolio/" : "/", // <-- base only for production
   server: {
     host: "::",
     port: 8080,
